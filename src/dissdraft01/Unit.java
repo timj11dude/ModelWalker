c08033b9160c1fd46ba1790866980f339a665597
eeue74 <eeue74@OADS20642.ad.bangor.ac.uk>
2014-11-13 11:26:01 +0000

    Re-factored the code for calculating movement, by introducing a new method for calculating the heuristic distance to the target, and storing the distance of each cell by trial and error through a for loop for the 3x3 grid of cells available for movement around the Unit.

--- src/dissdraft01/Unit.java
+++ src/dissdraft01/Unit.java
@@ -69,79 +69,36 @@ public class Unit extends GridReference
             /*
             *Check if the distance is shortest in which cell near the Unit
             */
-            double pos00 = (Math.sqrt(((Math.pow(((this.getX() - 1) - this.dest.getX()), 2) + Math.pow(((this.getY() - 1) - this.dest.getY()), 2))))) * Math.sqrt(2);
-            double pos01 = Math.sqrt(((Math.pow(((this.getX()) - this.dest.getX()), 2) + Math.pow(((this.getY() - 1) - this.dest.getY()), 2))));
-            double pos02 = (Math.sqrt(((Math.pow(((this.getX() + 1) - this.dest.getX()), 2) + Math.pow(((this.getY() - 1) - this.dest.getY()), 2))))) * Math.sqrt(2);
-            double pos10 = Math.sqrt(((Math.pow(((this.getX() - 1) - this.dest.getX()), 2) + Math.pow(((this.getY()) - this.dest.getY()), 2))));
-            double pos12 = Math.sqrt(((Math.pow(((this.getX() + 1) - this.dest.getX()), 2) + Math.pow(((this.getY()) - this.dest.getY()), 2))));
-            double pos20 = (Math.sqrt(((Math.pow(((this.getX() - 1) - this.dest.getX()), 2) + Math.pow(((this.getY() + 1) - this.dest.getY()), 2))))) * Math.sqrt(2);
-            double pos21 = Math.sqrt(((Math.pow(((this.getX()) - this.dest.getX()), 2) + Math.pow(((this.getY() + 1) - this.dest.getY()), 2))));
-            double pos22 = (Math.sqrt(((Math.pow(((this.getX() + 1) - this.dest.getX()), 2) + Math.pow(((this.getY() + 1) - this.dest.getY()), 2))))) * Math.sqrt(2);
-            
-            if (pos00 <= pos01 && pos00 <= pos02 && pos00 <= pos10 && pos00 <= pos12 && pos00 <= pos20 && pos00 <= pos21 && pos00 <= pos22)
+            double shortest = Double.MAX_VALUE;
+            GridReference pos = new GridReference(0,0);
+            for (int x = -1; x < 2; x++)
             {
-                this.setX(this.getX() - 1);
-                this.setY(this.getY() - 1);
-                return true;
+                for (int y = -1; y < 2; y++)
+                {
+                    double testLength = heuristic((this.getX() + x), (this.getY() + y));
+                    if (testLength < shortest) {
+                        shortest = testLength;
+                        try
+                        {
+                        pos = new GridReference((this.getX() + x), (this.getY() + y));
+                        }
+                        catch (NullPointerException e) {
+                            
+                        }
+                        
+                    }
+                }
             }
-            else if (pos01 <= pos02 && pos01 <= pos10 && pos01 <= pos12 && pos01 <= pos20 && pos01 <= pos21 && pos01 <= pos22)
-            {
-                this.setY(this.getY() - 1);
-                return true;
-            }
-            else if (pos02 <= pos10 && pos02 <= pos12 && pos02 <= pos20 && pos02 <= pos21 && pos02 <= pos22)
-            {
-                this.setX(this.getX() + 1);
-                this.setY(this.getY() - 1);
-                return true;
-            }
-            else if (pos10 <= pos12 && pos10 <= pos20 && pos10 <= pos21 && pos10 <= pos22)
-            {
-                this.setX(this.getX() - 1);
-                return true;
-            }
-            else if (pos12 <= pos20 && pos12 <= pos21 && pos12 <= pos22)
-            {
-                this.setX(this.getX() + 1);
-                return true;
-            }
-            else if (pos20 <= pos21 && pos20 <= pos22)
-            {
-                this.setX(this.getX() - 1);
-                this.setY(this.getY() + 1);
-                return true;
-            }
-            else if (pos21 <= pos22)
-            {
-                this.setY(this.getY() + 1);
-                return true;
-            }
-            else
-            {
-                this.setY(this.getY() + 1);
-                this.setX(this.getX() + 1);
-                return true;
-            }
-            
-            
-            /*
-             *Check if the distance along the Y-axis is longer than on the X-axis
-             */
-
-            /*if (Math.abs(this.getX() - dest.getX()) < Math.abs(this.getY() - dest.getY()))
-            {
-                //Move one step in the direction towards the target
-                //System.out.println("Y>X" + (this.getY() - dest.getY() - (this.getY() - dest.getY() - 1)));
-                this.setY(this.getY() + (this.getY() - dest.getY() - (this.getY() - dest.getY() - 1)));
-                return true;
-            }
-            else
-            {
-                //System.out.println("X>Y" + (this.getX() - dest.getX() - (this.getX() - dest.getX() - 1)));
-                this.setX(this.getX() + ((this.getX() - dest.getX()) - (this.getX() - dest.getX() - 1)));
-                return true;
-            }
-                    */
+            this.setX(pos.getX());
+            this.setY(pos.getY());
+            return true;
         }
+    }
+    private double heuristic(int x, int y)
+    {
+        double D = 1.0;
+        double dx = Math.abs(x - dest.getX());
+        double dy = Math.abs(y - dest.getY());
+        return D = D * (dx + dy);
     }
 }
