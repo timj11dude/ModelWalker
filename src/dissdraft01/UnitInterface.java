ff90b991abb46670942069f0ab9dcd4c823eccdf
tim <tim@192.168.1.7>
2015-01-22 16:03:57 +0200

    Created Interface for any new descriptions for Units that may traverse the map.

---
+++ src/dissdraft01/UnitInterface.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dissdraft01;
+
+/**
+ *
+ * @author tim
+ */
+public interface UnitInterface {
+    // constant declarations, if any
+
+   // method signatures
+   
+   void setCurr(GridReference curr);
+   
+   void setDest(GridReference dest);
+   Boolean move();
+           
+   
+   
+   // more method signatures
+}
