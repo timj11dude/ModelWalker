e833c856c428a08848484a8a76cea93d6bde29e9
eeue74 <eeue74@OAALSLS04.ad.bangor.ac.uk>
2015-04-26 12:17:59 +0100

    Small change, still needs work to be fully implimented.

--- src/test/WalkerTester.java
+++ src/test/WalkerTester.java
@@ -8,6 +8,7 @@ package test;
 import dissdraft01.walkers.WalkerAngle;
 import dissdraft01.Grid;
 import dissdraft01.GridReference;
+import dissdraft01.walkers.Walker;
 /**
  *
  * @author tim
@@ -19,7 +20,7 @@ public class WalkerTester {
      */
     public static void main(String[] args) {
         Grid grid = new Grid();
-        WalkerAngle walker = new WalkerAngle(new GridReference(1, 1), new GridReference(1, 10), grid);
+        Walker walker = new WalkerAngle(new GridReference(1, 1), new GridReference(1, 10), grid);
         System.out.println("Distance to 1, 2: " +  walker.distance(1, 1, 1, 2));
         System.out.println("Distance to 2, 2: " +  walker.distance(1, 1, 2, 2));
         System.out.println("Distance to 0, 2: " +  walker.distance(1, 1, 0, 2));
@@ -31,10 +32,6 @@ public class WalkerTester {
         System.out.println("Distance gained from 1, 2: " +  (walker.distance(1, 10) - walker.distance(1, 2, 1, 10)));
         System.out.println("Distance gained from 2, 2: " +  (walker.distance(1, 10) - walker.distance(2, 2, 1, 10)));
         System.out.println("Distance gained from 0, 2: " +  (walker.distance(1, 10) - walker.distance(0, 2, 1, 10)));
-        
-        System.out.println("Distance weighted from 1, 2: " +  walker.weighted(1, 2));
-        System.out.println("Distance weighted from 2, 2: " +  walker.weighted(2, 2));
-        System.out.println("Distance weighted from 0, 2: " +  walker.weighted(0, 2));
         
         System.out.println("Angle dif from 1, 2: " + walker.angle(1, 2));
         System.out.println("Angle dif from 2, 2: " + walker.angle(2, 2));
